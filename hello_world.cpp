36539d0df2e049167856d5499ba6f9a2585b5d8c
Ubuntu <ubuntu@ip-172-31-91-123.ec2.internal>
2020-08-04 20:01:52 +0000

    Added in hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2020 Marcus A. Mosley All rights reserved
+//
+// Created by Marcus A. Mosley
+// Created on August 2020
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main()  {
+    std::cout << "Hello, World!" << std::endl;
+}
